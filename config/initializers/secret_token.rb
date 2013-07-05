baaa1ea48f4bcd3fdbe71eb688823c2eb76a7a45
tuyennguyencanada <tuyennguyencanada@gmail.com>
2013-07-05 08:12:10 -0600

    Creating ss project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Siliconspots::Application.config.secret_key_base = '743ba43f5a5f7fc35b57f092199faa7af822c679adb1b5e28e97c16d10c1566fcdf1fb04fe2ea3c81ec84b7c64b5d7e17535125d8e7fc0349db98aed727d11d8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Siliconspots::Application.config.session_store :cookie_store, key: '_siliconspots_session'
